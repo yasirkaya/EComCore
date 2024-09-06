b83f74a4559afea57851b0f6452f614aef11a5a2
Yasir Kaya <kayamuhammedyasir@gmail.com>
2024-09-06 18:41:39 +0300

    migration

---
+++ Backend/EComCore.Infrastructure/Migrations/20240906151542_changeAttribute.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EComCore.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeAttribute : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Shipments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Reviews",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Payments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "OrderItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Members",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Addresses",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Shipments");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Members");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Addresses");
+        }
+    }
+}
